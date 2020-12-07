3229debcab4f36e8e6fca7be18919c06f69b5996
DSauthier <danielsauthier@live.com>
2020-12-07 17:21:25 -0300

    changed index

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express()
-const port = 3100;
+const port = 3000;
 
 
 
@@ -8,6 +8,9 @@ const port = 3100;
 app.get('/', (req, res) => res.sendFile('/home/ec2-user/node-demo/start.html'));
 
 
+// app.get('/', (req, res) => res.sendFile('/Users/danielSauthier/Desktop/sideProjects/node-demo/start.html'));
+
+app.use(express.static('/'));
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
